4b475e68b75644f2e7f8f5b29b8772f82041beb1
Gunel Huseynova <gorellik246@gmail.com>
2021-02-09 13:57:43 +0300

    feat: add custom select functionality

---
+++ src/js/main.js
@@ -0,0 +1,59 @@
+function createCustomSelect() {
+    document
+        .querySelector(".f-select__btn")
+        .addEventListener("click", function() {
+            const parentEl = this.parentElement;
+            parentEl.classList.toggle("f-select--opened");
+        });
+
+    for (const selectItem of document.querySelectorAll(".f-select__item ")) {
+        selectItem.addEventListener("click", function(e) {
+            e.stopPropagation();
+
+            const el = e.currentTarget;
+
+            if (!el.classList.contains("selected")) {
+                if (el.parentNode.querySelector(".f-select__item.selected")) {
+                    el.parentNode
+                        .querySelector(".f-select__item.selected")
+                        .classList.remove("selected");
+                }
+
+                el.classList.add("selected");
+                el
+                    .closest(".f-select")
+                    .querySelector(".f-select__txt").textContent = el.querySelector(
+                        ".f-select__item-label"
+                    ).textContent;
+                el.closest(".f-select").classList.remove("f-select--opened");
+            }
+        });
+    }
+
+    window.addEventListener("click", function(e) {
+        const select = document.querySelector(".f-select");
+        if (!select.contains(e.target)) {
+            select.classList.remove("f-select--opened");
+        }
+    });
+}
+
+function createCustomInputFile() {
+    const fileInput = document.querySelector(".form__input--upload");
+
+    fileInput.addEventListener("change", function() {
+        const currFiles = this.files;
+        if (currFiles.length !== 0) {
+            document.querySelector(
+                ".form__label--upload .form__label-txt"
+            ).textContent = currFiles[0].name;
+        } else {
+            document.querySelector(
+                ".form__label--upload > .form__label-txt"
+            ).textContent = "No files uploaded";
+        }
+    });
+}
+
+createCustomSelect();
+createCustomInputFile();
